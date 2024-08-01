cdb99c5ffa90d542a61d08175e7d7c0a6bb1be7e
Justin Meyer <justinbmeyer@gmail.com>
2024-07-31 21:30:06 -0500

    adds adjusted-story-points service

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { fetchTokenWithAccessCode } = require('./helper')
 const cors = require('cors');
 const path = require('path');
 
+const stats = require("./stats");
 // configurations
 dotenv.config()
 
@@ -61,6 +62,62 @@ app.get('/access-token', async (req, res) => {
         });
     }
 });
+
+const requiredFields = {
+    storyPointsMedian: function(value){
+        if(value === undefined) {
+            return {message: "storyPointsMedian is undefined"}
+        }
+        if(typeof value !== "number") {
+            return {message: "storyPointsMedian is not a number"}
+        }
+        if(value < 0 ) {
+            return {message: "storyPointsMedian is negative"}
+        }
+    },
+    storyPointsConfidence: function(value){
+        if(value === undefined) {
+            return {message: "storyPointsConfidence is undefined"}
+        }
+        if(typeof value !== "number") {
+            return {message: "storyPointsConfidence is not a number"}
+        }
+        if(value < 0 ) {
+            return {message: "storyPointsConfidence is negative"}
+        }
+        if(value > 100) {
+            return {message: "storyPointsConfidence is greater than 100"}
+        }
+    }
+}
+function validateAdjustedStoryPoints(req, res, next) {
+    const errors = [];
+    for(let requiredField in requiredFields) {
+        let error = requiredFields[requiredField](req.body[requiredField]);
+        if(error) {
+            errors.push(error)
+        }
+    }
+    if(errors.length) {
+        res.status(400).json({errors})
+    } else {
+        next();
+    }
+}
+
+app.post('/adjusted-story-points', validateAdjustedStoryPoints, (req, res) => {
+
+
+    const extraStoryPoints = stats.estimateExtraPoints(req.body.storyPointsMedian, req.body.storyPointsConfidence, req.body.riskThreshold || 80)
+    const adjustedStoryPoints = req.body.storyPointsMedian + extraStoryPoints;
+    // Respond with a status message
+    res.status(200).json({ 
+        adjustedStoryPoints, 
+        extraStoryPoints,
+        roundedExtraStoryPoints: Math.round(extraStoryPoints),
+        roundedAdjustedStoryPoints: Math.round(adjustedStoryPoints)
+    });
+  });
 
 // Start server
 app.listen(port, () => console.log(`Server is listening on port ${port}!`))
